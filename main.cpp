cb1f0fb1bf38b6e5fe2d3fc68957f5d6ca2c7a0e
Jonathan Little <Jonjy@users.noreply.github.com>
2019-01-23 17:17:12 -0800

    made a branch added a function

--- main.cpp
+++ main.cpp
@@ -6,15 +6,25 @@ using namespace std;
 
 int hello1Function(string name1);
 int hello2Function(string name2);
+
 int main(){
-string name1="your name1";
-hello1Function(name1);
-string name2="your name2";
-hello2Function(name2);
+	string name1="your name1";
+	hello1Function(name1);
+	string name2="your name2";
+	hello2Function(name2);
 return 0;
 }
 
 /*
- * hello1Function.cpp 
+ * hello1Function.cpp
  *
  */
+ 
+ int hello1Function(string name1){
+ 	cout << "hello World " << name1 << endl;
+ 	return 0;
+ }
+
+
+ 
+ 
