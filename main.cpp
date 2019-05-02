eaff17a5ef3fadad970f65058f095e9a071bd7ea
Jonathan Little <jondlittle12@gmail.com>
2019-05-01 18:28:04 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,68 +8,116 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 #include <iostream>
 #include <stdlib.h>
-#include <time.h>
 #include <vector>
-#include <algorithm>
 #include <iterator>
+#include <ctime>
 
 using namespace std;
 
 
+clock_t runner();
 
-void quick(vector * mine, iterator start, iterator finish);
+void quick(vector<int> * mine, vector<int>::iterator front, vector<int>::iterator finish);
 
-void quickHelp();
+int pivot1(vector<int> * mine, vector<int>::iterator start, vector<int>::iterator finish);
 
-int pivot(vector * mine, iterator start, iterator finish);
+int pivot2(vector<int> * mine, vector<int>::iterator start, vector<int>::iterator finish);
+
+int pivot3(vector<int> * mine, vector<int>::iterator start, vector<int>::iterator finish);
 
 
 
+int main(){
+    clock_t t = 0;
+    int count = 1000;
+    for(int i = 0; i<count;i++){
+        t += runner();
+    }
+    t = t/count;
+    cout <<t;
+}
 
-int main()
-{
+
+clock_t runner(){
+    int count = 10000;
+    
     vector<int> mine;
     
     srand (time(nullptr));
     
-    for(int i =0; i<3000; i++){
-        int b = rand() %3000 + 1;
-        mine.push_back(b);
+    for(int i =0; i<count; i++){
+        //int b = rand() %3000 + 1;
+        //mine.push_back(i);
+        mine.insert(mine.begin(), i);
     }
     
-    for(int i =0; i<1000;i++){
-        cout << mine[i] << endl;
+    for(int i =0; i<10;i++){
+        cout << mine[i] << ", ";
+        if(((i+1)%25)==0){
+            cout<<endl;
+        }
     }
     
+    cout << endl;
+    clock_t t = clock();
     sort(mine.begin(), mine.end());
+    t = clock() - t;
     
-    for(int i =0; i<1000;i++){
-        cout << mine[i] << endl;
+    
+    for(int i =0; i<10;i++){
+        cout << mine[i] << ", ";
     }
+        for(int i =10; i>0;i--){
+        cout << mine[mine.size() -i] << ", ";
+    }
+    
+    for(int i = 0; i<(count-1); i++){
+        if(mine[i]>mine[i+1]){
+            cout<<"fail at " << mine[i];
+        }
+    }
+    
+    cout<<endl <<endl <<t<<endl ;
+    return t;
 }
 
 
-void quick(vector * mine, iterator start, iterator finish){
-    if(start == finish){
+void quick(vector<int> * mine, vector<int>::iterator front, vector<int>::iterator finish){
+    if(front == finish){
         return;
     }
-    iterator one = start;
-    iterator two = finish;
-    int pivot = pivot(mine, start, finish);
+    vector<int>::iterator one = front;
+    vector<int>::iterator two = finish;
+    int piv = pivot1(mine, front, finish);
     
-    while(start != finish){
-        
+    while((one+1) != two){
+        if(*one > piv){
+            int temp = *one;
+            *one = *two;
+            *two = temp;
+            two--;
+        }else{
+            one++;   
+        }
     }
-    
-    quick(mine, start, one);
-    quick(mine, two, finish)
-    
-    
+    quick(mine, front, one);
+    quick(mine, two, finish);
 }
 
-int pivot(vector * mine, iterator start, iterator finish){
-    return *start;
+int pivot1(vector<int> * mine, vector<int>::iterator start, vector<int>::iterator finish){
+    return *(start);
+}
+
+int pivot2(vector<int> * mine, vector<int>::iterator start, vector<int>::iterator finish){
+    int dist = distance(start, finish);
+    dist = dist/2;
+    return *(start+dist);
 }
 
 
-
+int pivot3(vector<int> * mine, vector<int>::iterator start, vector<int>::iterator finish){
+    int dist = distance(start, finish);
+    srand (time(nullptr));
+    int b = rand() % dist;
+    return *(start+b);
+}
